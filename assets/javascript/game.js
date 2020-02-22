e62b60aecc81344448982bf60eaf5a8f0fb69693
Lisey Panda <FF7JE0@Elises-MacBook-Pro.local>
2020-02-22 10:14:51 -0500

    added cards for guessing letters in index.html. initialized arrays in game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+//create array for Characters in OW
+//create array for User Guesses
+//Create array for Incorrect Answers
+var characters = ["tracer", "orisa", "soldier 76", "widowmaker", "junkrat", "roadhog", "d va", "sombra", "reaper", "doomfist"];
+var userAnswer = [];
+var incorrectAnswer = [];
+
+//length of Characters array
+var length = characters.length;
+console.log(characters);
+//generate random character with the correct length of '_'
+for (var i = 0; i < length; i++){
+
+}
+
+document.getElementById("#placeholder").innerHTML =
